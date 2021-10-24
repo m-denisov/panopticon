a263d47ec6bb0acf7dfdc9d82c9ff0098e4ca89f
mezgang <mezgang@gmail.com>
2021-10-24 03:25:03 +0300

    add path for mac

--- src/main/java/com/group/original/panopticon/App.java
+++ src/main/java/com/group/original/panopticon/App.java
@@ -1,50 +1,39 @@
 package com.group.original.panopticon;
 
 import com.group.original.panopticon.exception.ExceptionHandler;
-import com.group.original.panopticon.file.attrs.Size;
-import com.group.original.panopticon.file.attrs.Time;
 import com.group.original.panopticon.file.system.Differences;
 import com.group.original.panopticon.file.system.DirectoryStamp;
 import com.group.original.panopticon.output.manager.ConsoleOutputManager;
-import org.apache.commons.codec.digest.DigestUtils;
 
 import java.io.*;
-import java.nio.file.Files;
-import java.nio.file.LinkOption;
 import java.nio.file.Path;
-import java.nio.file.attribute.BasicFileAttributes;
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
-import java.time.Instant;
-import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.List;
 
 public class App {
 
 	public static void main( String[] args ) {
-        //Path path = Path.of("C:\\Users\\m.denisov\\Documents\\Проекты\\станые, взамен утерянных\\Татхимфармпрепараты\\test");
+        Path wsecondStampPath = Path.of("C:\\Users\\m.denisov\\stamps\\2.txt");
 
-        Path secondStampPath = Path.of("C:\\Users\\m.denisov\\stamps\\2.txt");
+        Path wfirst = Path.of("C:\\Users\\m.denisov\\Documents\\Оборудование");
+        Path wsecond = Path.of("D:\\test");
+        Path wtestFile = Path.of("C:\\Users\\m.denisov\\Documents\\временные\\Без имени 1.odt");
+        Path wthird = Path.of("F:\\1EN-TraceWay\\2.9. Департамент качества\\Проекты");
 
-        Path first = Path.of("C:\\Users\\m.denisov\\Documents\\Оборудование");
-        Path second = Path.of("D:\\test");
-        Path testFile = Path.of("C:\\Users\\m.denisov\\Documents\\временные\\Без имени 1.odt");
-        Path third = Path.of("F:\\1EN-TraceWay\\2.9. Департамент качества\\Проекты");
+        Path mfirst = Path.of("/Users/westtochka/Documents/panopticonTest/first");
+        Path msecond = Path.of("/Users/westtochka/Documents/panopticonTest/second");
 
 //        DirectoryStamp firstStamp = DirectoryStamp.stampOf(first);
-        DirectoryStamp secondStamp = DirectoryStamp.stampOf(second);
+        DirectoryStamp secondStamp = DirectoryStamp.stampOf(wsecond);
 //        Differences differences = new Differences(firstStamp, secondStamp);
 //        System.out.println(differences);
 
-//        try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(secondStampPath.toString()))) {
+//        try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(wsecondStampPath.toString()))) {
 //            oos.writeObject(secondStamp);
 //        } catch (IOException ioException) {
 //            ioException.printStackTrace();
 //        }
 
         DirectoryStamp stamp = null;
-        try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream(secondStampPath.toString()))) {
+        try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream(wsecondStampPath.toString()))) {
             stamp = (DirectoryStamp) ois.readObject();
         } catch (FileNotFoundException e) {
             e.printStackTrace();
