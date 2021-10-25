32e06e7f935453923d4449b81886f1a7af00fc86
mezgang <mezgang@gmail.com>
2021-10-25 13:10:10 +0300

    add method formattedDate in class Time

--- src/main/java/com/group/original/panopticon/file/attrs/Time.java
+++ src/main/java/com/group/original/panopticon/file/attrs/Time.java
@@ -7,13 +7,18 @@ import java.time.format.DateTimeFormatter;
 
 public class Time {
     private static final ZoneId ZONE_ID = ZoneId.of("Europe/Moscow");
-    private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:dd");
+    private static final DateTimeFormatter DATE_TIME_FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:dd");
+    private static final DateTimeFormatter TIME_FORMATTER = DateTimeFormatter.ofPattern("yyy-MM-dd");
 
     public static LocalDateTime ofInstant(Instant instant) {
         return LocalDateTime.ofInstant(instant, ZONE_ID);
     }
 
     public static String formattedDateTime(LocalDateTime localDateTime) {
-        return localDateTime.format(FORMATTER);
+        return localDateTime.format(DATE_TIME_FORMATTER);
+    }
+
+    public static String formattedDate(LocalDateTime localDateTime) {
+        return localDateTime.format(TIME_FORMATTER);
     }
 }
